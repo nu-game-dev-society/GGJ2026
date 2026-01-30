0c87f5eb548a325d81eacc8352ca7a31dcf63263
Game Jam <gamejam@hal.local>
2026-01-30 18:52:49 +0000

    Initial input logic

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,98 @@
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    #region Serialised Fields
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpHeight = 1.5f;
+    [SerializeField] private float gravity = -9.81f;
+
+    [Header("Look")]
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private float mouseSensitivity = 100f;
+    [SerializeField] private float maxLookAngle = 80f;
+
+    [Header("Required components")]
+    [SerializeField] private CharacterController controller;
+    #endregion
+
+    private InputSystem_Actions inputActions;
+
+    private Vector2 moveInput;
+    private Vector2 lookInput;
+    private Vector3 velocity;
+
+    #region Input callbacks
+    private void OnMovePerformed(CallbackContext ctx)
+    {
+        moveInput = ctx.ReadValue<Vector2>();
+    }
+
+    private void OnMoveCancelled(CallbackContext ctx)
+    {
+        moveInput = Vector2.zero;
+    }
+
+    private void OnJumpPerformed(CallbackContext ctx)
+    {
+        Jump();
+    }
+    #endregion
+
+    #region Unity Methods
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        inputActions = new InputSystem_Actions();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Player.Enable();
+
+        inputActions.Player.Move.performed += OnMovePerformed;
+        inputActions.Player.Move.canceled += OnMoveCancelled;
+
+        inputActions.Player.Jump.performed += OnJumpPerformed;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.Disable();
+    }
+
+    private void Update()
+    {
+        Move();
+        ApplyGravity();
+    }
+    #endregion
+
+    private void Move()
+    {
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        if (controller.isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
