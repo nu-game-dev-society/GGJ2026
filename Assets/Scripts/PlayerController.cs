db56988be1e1a41f191817a165296a3b4901e4fd
Joseph Whitehead <joseph.whitehead101@gmail.com>
2026-01-31 12:57:11 +0000

    jump anim

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -122,5 +122,7 @@ public class PlayerController : MonoBehaviour
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
-    }
+
+        animator.SetFloat("YVel", controller.velocity.y);
+    } 
 }
